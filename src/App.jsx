e95de2fd6f31adfa3de70cbacb59ee5adf4d9d72
Alessandro Ledda <aleledda75@icloud.com>
2026-04-28 11:08:32 +0200

    feat mileston 1 in file app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,48 @@
-
+import { useEffect, useState } from 'react';
 
 function App() {
 
+  // setto var di stato per gestione input
+  const [query, setQuery] = useState("");
+
+  // setto var di stato per gestione input prodotti
+  const [products, setProducts] = useState([]);
+
+  // controllo oggetto product
+  console.log(products)
+
+  useEffect(() => {
+    if (!query.trim()) {
+      setProducts([]);
+      return;
+    }
+    // chiata Api
+    fetch(`http://localhost:3333/products?search=${query}`)
+      .then(res => res.json())
+      .then(data => setProducts(data))
+      .catch(error => console.error(error))
+      .finally()
+
+  }, [query]);
+
+
+
   return (
     <>
-
+      <h1>useCallback</h1>
+      <input
+        type="text"
+        placeholder='cerca'
+        value={query}
+        onChange={e => setQuery(e.target.value)}
+      />
+      {products.length > 0 && (
+        <div>
+          {products.map(product => (
+            <p key={product.id}>{product.name}</p>
+          ))}
+        </div>
+      )}
     </>
   )
 }
