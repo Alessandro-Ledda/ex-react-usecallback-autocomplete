86ba0ad7f385e53a72a8ee9d94a01f3620dd1a38
Alessandro Ledda <aleledda75@icloud.com>
2026-04-28 12:13:38 +0200

    feat bonus in file app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,9 @@ function App() {
   // setto var di stato per gestione input prodotti
   const [products, setProducts] = useState([]);
 
+  // setto var di stato per gestione prodotto selezionato
+  const [selectedproduct, setSelectedProduct] = useState(null);
+
   // controllo oggetto product
   console.log(products)
 
@@ -40,7 +43,7 @@ function App() {
       // verifica delle chiamate ad ogni digitazione da tastiera di user
       console.log('richiesta api')
     } catch (error) {
-      (error => console.error(error));
+      console.error(error);
     }
   }
 
@@ -48,13 +51,25 @@ function App() {
   const debouncedFetchProducts = useCallback(
     debounce(fetchProducts, 500), [])
 
-
-
   useEffect(() => {
     debouncedFetchProducts(query);
   }, [query]);
 
+  // creo funzione per chiamata api per i dettagli prodotto
+  const fetchProductsDetails = async (id) => {
+    try {
+      const res = await fetch(`http://localhost:3333/products/${id}`);
+      const data = await res.json();
+      setSelectedProduct(data);
+      // pulizia dopo la selezione prodotto del campo
+      setQuery("");
+      setProducts([]);
 
+    } catch (error) {
+      console.error(error);
+    }
+
+  }
 
   return (
     <>
@@ -68,8 +83,20 @@ function App() {
       {products.length > 0 && (
         <div>
           {products.map(product => (
-            <p key={product.id}>{product.name}</p>
+            <p key={product.id}
+              onClick={() => fetchProductsDetails(product.id)}
+            >{product.name}</p>
           ))}
+        </div>
+      )}
+
+      {/* se esiste il mio select procedo con il codice */}
+      {selectedproduct && (
+        <div className='card'>
+          <h2>{selectedproduct.name}</h2>
+          {/* <img src={selectedproduct.image} alt={selectedproduct.name} /> */}
+          <p>{selectedproduct.description}</p>
+          <p>{selectedproduct.price}€</p>
         </div>
       )}
     </>
